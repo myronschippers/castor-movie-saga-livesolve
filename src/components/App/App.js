4ca90ea8c995c1998d4af56aacaf1a2992994eee
Myron <dubtwee@gmail.com>
2020-11-16 09:19:39 -0600

    creating initial pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+// CUSTOM COMPONENTS
+import HomePage from '../../pages/HomePage/HomePage';
+import DetailsPage from '../../pages/DetailsPage/DetailsPage';
+import AddMoviePage from '../../pages/AddMoviePage/AddMoviePages';
 
 class App extends Component {
   // Renders the entire app on the DOM
@@ -10,8 +15,10 @@ class App extends Component {
         <h1>Movies!</h1>
         <Router>
           {/* ADD PAGES! */}
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/details" component={DetailsPage} />
+          <Route exact path="/add-movie" component={AddMoviePage} />
         </Router>
-        <p>Empty Page</p>
       </div>
     );
   }
---
+++ src/pages/AddMoviePage/AddMoviePages.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddMoviePage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Add Movie</h2>
+      </div>
+    );
+  }
+}
+
+export default AddMoviePage;
