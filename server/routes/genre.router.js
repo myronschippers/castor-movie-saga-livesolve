7e21b8b171ea16939b2e64200d2034e722b83c56
Myron <dubtwee@gmail.com>
2020-11-16 10:09:46 -0600

    creating query to db for genres

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool')
+const pool = require('../modules/pool');
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  pool
+    .query(`SELECT * FROM "genres" ORDER BY "name" ASC;`)
+    .then((dbResponse) => {
+      res.send(dbResponse.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
